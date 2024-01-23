85a00279e5b0a46ad04ca2a95a538e67fed761c4
trueeth <trueeth@powerhouseit.org>
2024-01-23 12:48:50 +0900

    fix errors in tx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,19 +5,8 @@ import Layout from '../components/Layout'
 import Providers from '../Providers'
 import store from '../store/store'
 
-
-
-    
-    // Optional
-    appDescription: 'Your App Description',
-    appUrl: 'https://family.co', // your app's url
-    appIcon: 'https://family.co/logo.png', // your app's icon, no bigger than 1024x1024px (max. 1MB)
-  })
-)
-
 function MyApp(props: AppProps<{ initialReduxState: any }>) {
   const { pageProps, Component } = props
-
 
   return (
     <Providers store={store}>
